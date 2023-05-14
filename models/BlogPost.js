d0b062589aca7cffa469129111a94d0c0f2cb073
Elissa Maine <emaine97@yahoo.com>
2023-05-14 00:50:45 -0700

    added User BlogPost and Comment model

---
+++ models/BlogPost.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING(10000),
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    // date_created: {},
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogpost',
+  }
+);
+
+module.exports = BlogPost;
