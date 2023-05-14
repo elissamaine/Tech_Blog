32978d9e8ae0a7da59a2b8d5c5fca13da7d897f7
Elissa Maine <emaine97@yahoo.com>
2023-05-13 23:04:45 -0700

    added color pallet to css

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Model, DaqtaTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+user.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      
+    }
+   }
+)
