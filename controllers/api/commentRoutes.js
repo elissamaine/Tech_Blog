31428f5f4a43670aa709d00a0f7bfad88aa30c85
elissamaine <emaine97@yahoo.com>
2023-05-15 18:28:55 -0700

    added to comment routes and user Routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newCommentData = await Comment.create({
+      comment_text: req.body.comment_text,
+    });
+    res.status(200).json(newCommentData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.update(
+      {
+        comment_text: req.body.comment_text,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    await Comment.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json({ message: 'comment deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
