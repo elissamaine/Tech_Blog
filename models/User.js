481dd38f891fb6ee7ae321e43882c3b091faaa4d
Elissa Maine <emaine97@yahoo.com>
2023-05-16 01:35:04 -0700

    added login handlebars and updated user model and home routes

--- models/User.js
+++ models/User.js
@@ -19,6 +19,12 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique:true,
+    },
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
