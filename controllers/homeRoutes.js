9dae8e7e8440861a8de0291e58c653510cf134d4
elissamaine <emaine97@yahoo.com>
2023-05-18 14:17:14 -0700

    fixed profile handlebars so that user blog posts are displayed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,9 +33,9 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
     console.log(user);
-    
+
     res.render('profile', {
-      ...user,
+      user,
       logged_in: true,
     });
   } catch (err) {
