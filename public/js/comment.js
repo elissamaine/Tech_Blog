6f882329887e4502b99a9e5275fb4d15730d79e3
elissamaine <emaine97@yahoo.com>
2023-05-19 12:07:27 -0700

    fixed bug to add comments to posts

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,15 +2,20 @@ const postComment = async (event) => {
   event.preventDefault();
 
   const commentContent = document.querySelector('#comment-input').value.trim();
+  const commentPostId = window.location.pathname.split('/').pop();
 
   if (!commentContent) {
     alert('Please enter a comment.');
   }
-  console.log(commentContent);
+
+  const commentData = {
+    comment_text: commentContent,
+    post_id: commentPostId,
+  }
   
   const response = await fetch('/api/comments', {
     method: 'POST',
-    body: JSON.stringify({ commentContent }),
+    body: JSON.stringify(commentData),
     headers: { 'Content-Type': 'application/json' },
   });
 
@@ -18,6 +23,7 @@ const postComment = async (event) => {
     document.location.reload();
   } else {
     alert(response.statusText);
+    console.log(response);
   }
 };
 
